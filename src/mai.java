97259de83fcad5921f1e3aaf394cd59794d175a6
Bolot <bkuser13@gmail.com>
2018-06-09 16:13:30 -0500

    first selenium project

---
+++ src/mai.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class mai {
+public static void main(String[] args) {
+	
+
+	System.setProperty("webdriver.chrome.driver", "/Users/admin/Downloads/chromedriver");
+
+	WebDriver driver = new ChromeDriver();
+	
+	driver.get("http://google.com");
+}
+}
